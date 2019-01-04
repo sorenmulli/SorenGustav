067721dab919c87a255a9226b2baed75436d6489
Søren Winkel Holm <s183911@student.dtu.dk>
2019-01-04 19:29:15 +0100

    Hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SorenGustav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
